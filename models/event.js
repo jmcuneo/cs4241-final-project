cb0db71d1a2c751761aa403ef80a8fc92ba862b3
AlexanderBeck0 <25886582+AlexanderBeck0@users.noreply.github.com>
2024-04-16 13:51:15 -0400

    Lots of incomplete schema integration. Permissions have NOT been implemented

--- models/event.js
+++ models/event.js
@@ -5,49 +5,119 @@ const { Schema, model, SchemaTypes } = mongoose;
 // Hasn't be debugged, tested, or anything of the like. 
 
 /**
- * @author Whoever ends up implmeneting this schema
- * @todo Implement the schema
+ * @author Alexander Beck
+ * @todo Validate attendees, check if user has permission before creating event
  */
 const eventSchema = new Schema({
-    eventName: {
+    name: {
         type: String,
-        minLength: 1,
+        minLength: [1, 'Event name must be at least 1 character long!'],
         required: true,
     },
+    date: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+        minLength: [1, 'Event location must be at least 1 character long!'],
+        required: true
+    },
+    creator: {
+        type: SchemaTypes.ObjectId,
+        ref: 'User',
+        validate: {
+            validator: function () {
+                // Ensure that creator has PERMISSIONS.CREATE_EVENT or is an admin
+                return true;
+            },
+            // TODO: Make this message valid
+            message: props => `The creator, ${props.value}, does not have permission to create events!`
+        },
+        required: true
+    },
+    // TODO: Validate that attendees is set up properly (I have no idea if it is properly referencing a User)
     attendees: [{
-        user: {
+        guest: {
             type: SchemaTypes.ObjectId,
             ref: 'User',
             required: true // Is this necessary?
-        }
-    }]
+        },
+        inviter: {
+            type: SchemaTypes.ObjectId,
+            ref: 'User',
+            validate: {
+                validator: function () {
+                    // Ensure that the inviter is in the allowed inviters,
+                    // has PERMISSIONS.INVITE_TO_ALL_EVENTS, or the inviter is an admin.
+                    // Also ensure that the inviter isn't above their invite limit
+                    return true
+                },
+                // TODO: Make this message valid
+                message: props => `The inviter, ${props.value}, does not have permission to invite people!`
+            }
+        },
+    }],
+    guestLimit: {
+        type: Number,
+        min: [0, 'Guest Limit must be greater than 0, got {VALUE}'],
+        inviterLimit: {
+            type: Number,
+            min: [0, 'Inviter Limit must be greater than 0, got {VALUE}'],
+            required: false
+        },
+    }
+}, { timestamps: true });
+
+// TODO: Implement this
+eventSchema.virtual('guestCount').get(function () {
+    return this.attendees.length;
 });
 
 /**
- * @author Whoever ends up implmeneting this schema
+ * @author Alexander beck
  * @example
  *          // Inserting data
+ * 
+ *          // Make sure to verify that the user has permissiosn before creating the event!
+ *          
  *          const event = await Event.create({
- *              fieldOne: 'FieldOnes data',
- *              boolOne: false,
- *              arrOne: ['item1', 'item2']
+ *              name: 'My Event',
+ *              date: new Date(2024, 11, 25),
+ *              location: 'Unity 520',
+ *              creator: user._userId,
  *          });
  * 
  *          // Updating data
- *          event.fieldOne = 'FieldOnes updated data';
- *          await event.save();
+ *          event.location = 'Unity 500';
+ *          await event.save(); // When you are finished making changes
+ *  
+ *          // Bulk Updating Data 
+ *          const christmasParty = await Event.create({
+ *              name: 'Christmas Party',
+ *              date: new Date(2024, 11, 25),
+ *              location: 'Unity 520',
+ *              creator: user
+ *          });
  * 
+ *          christmasParty.attendees.addToSet(
+ *              { guest: user2, inviter: user },
+ *              { guest: user3, inviter: user });
+ *          await christmasParty.save();
+ *  
  *          // Finding data
  *          const event = await Event.findById("{id}").exec();
- * 
+ *          // or
+ *          const event = await Event.findOne({_id: {id}})
+ *          
  *          // Getting only specific data
  *          const event = await Event.findById("{id}", "fieldOne boolOne").exec();
  * 
  *          // Deleting data
  *          const event = await Event.deleteOne({fieldOne: "FieldOne data"});
  *          const event = await Event.deleteMany({fieldOne: "FieldOne data"});
- * @see {@link eventSchema}
+ * @see {@link eventSchema}, {@link Date}
  */
-const Event = model('Event', eventSchema);
+const Event = model('Event', eventSchema, 'Events');
 
 export default Event;
--- models/user.js
+++ models/user.js
@@ -5,42 +5,125 @@ const { Schema, model } = mongoose;
 // Hasn't be debugged, tested, or anything of the like. 
 
 /**
- * @author Whoever ends up implmeneting this schema
- * @todo Implement the schema
+ * @author Alexander Beck
+ * @example 
+ *      ACCOUNT_TYPE.genpop = 'genpop'
+ *      ACCOUNT_TYPE.admin = 'admin'
+ *      ACCOUNT_TYPE.other = 'other'
  */
-const userSchema = new Schema({
-    name: {
-        type: String,
-        minLength: 1,
-        required: true,
-    }
+export const ACCOUNT_TYPE = Object.freeze({
+    GENPOP: 'GENPOP',
+    ADMIN: 'ADMIN',
+    OTHER: 'OTHER'
 });
 
 /**
- * @author Whoever ends up implmeneting this schema
+ * @author Alexander Beck
+ * 
+ * May or may not end up using these. Just an idea.
+ */
+export const PERMISSIONS = Object.freeze({
+    EDIT_ALL_EVENTS: 'EDIT_ALL_EVENTS',
+    EDIT_ALL_USERS: 'EDIT_ALL_USERS',
+    REMOVE_ALL_USERS: 'REMOVE_ALL_USERS',
+    INVITE_TO_ALL_EVENTS: 'INVITE_TO_ALL_EVENTS',
+    GIFT_ADMIN: 'GIFT_ADMIN',
+    CREATE_EVENT: 'CREATE_EVENT',
+});
+
+/**
+ * @author Alexander Beck
+ * @JackWeinstein808 Username uniqueness checking (consider using the User.exists() method)
+ * @todo implement permissions
+ * 
+ * Defaults: accountType = {@linkcode ACCOUNT_TYPE.genpop}
+ * 
+ * Not required: username, gender
+ */
+const userSchema = new Schema({
+    _id: {
+        type: mongoose.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId(),
+        required: true
+    },
+    firstName: {
+        type: String,
+        minLength: 1,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        minLength: 1,
+        required: true,
+    },
+    username: {
+        type: String,
+        minLength: 1,
+        required: false,
+    },
+    gender: {
+        type: String,
+        required: false,
+    },
+    accountType: {
+        type: String,
+        default: ACCOUNT_TYPE.GENPOP,
+        enum: {
+            values: Object.values(ACCOUNT_TYPE),
+            message: 'VALUE is not a supported account type.'
+        },
+        required: true,
+    },
+    // TODO: Implement permissions (make them actually matter)
+    permissions: [{
+        permission: {
+            type: String,
+            enum: {
+                values: Object.values(PERMISSIONS),
+                message: 'VALUE is not a supported permission'
+            },
+            required: false,
+        }
+    }],
+}, { timestamps: true });
+
+/**
+ * Creates a fullName field that is not actually stored in the DB, but can still be accessed.
+ * Note: Because they are not stored in the DB, you cannot query with them.
+ */
+userSchema.virtual('fullName').get(function () {
+    return this.firstName + ' ' + this.lastName;
+});
+
+/**
+ * @author Alexander Beck
+ * @requires firstName, lastName
  * @example
  *          // Inserting data
  *          const user = await User.create({
- *              fieldOne: 'FieldOnes data',
- *              boolOne: false,
- *              arrOne: ['item1', 'item2']
+ *              firstName: 'Jane',
+ *              lastName: 'Doe',
+ *              gender: 'Female',
  *          });
  * 
  *          // Updating data
- *          user.fieldOne = 'FieldOnes updated data';
+ *          user.firstName = 'John';
+ *          user.lastName = 'Smith';
+ *          user.accountType = ACCOUNT_TYPE.ADMIN;
  *          await user.save();
  * 
  *          // Finding data
  *          const user = await User.findById("{id}").exec();
  * 
  *          // Getting only specific data
- *          const user = await User.findById("{id}", "fieldOne boolOne").exec();
+ *          const user = await User.findById("{id}", "firstName lastName").exec();
  * 
  *          // Deleting data
- *          const user = await User.deleteOne({fieldOne: "FieldOne data"});
- *          const user = await User.deleteMany({fieldOne: "FieldOne data"});
+ *          // If the query to delete data is from a user, remember to check if they have the permissions!
+ *          const user = await User.deleteOne({firstName: "Jane"});
+ *          const user = await User.deleteMany({firstName: "Jane"});
  * @see {@link userSchema}
  */
-const User = model('User', userSchema);
+const User = model('User', userSchema, 'Users');
 
 export default User;
