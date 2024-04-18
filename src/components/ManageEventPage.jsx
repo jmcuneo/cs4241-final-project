b181b130fe8e9b21f5a815f4ca464fd6233dafa0
Perez0002 <op90210@gmail.com>
2024-04-18 19:47:40 -0400

    GuestListComponent.jsx table with temp arr. added to EventPage.jsx and ManageEventPage.jsx

--- src/components/ManageEventPage.jsx
+++ src/components/ManageEventPage.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useRef } from 'react';
 import { useNavigate } from "react-router-dom";
+import GuestListComponent from "./GuestListComponent.jsx";
 
 function ManageEventPage() {
   
   return(
     <div>
-
+      <GuestListComponent/>
     </div>
   );
 }
