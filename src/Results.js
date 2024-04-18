b0e747052e0ada1ae6bb197c38e69449f7508898
Jolene Pern <jpern@wpi.edu>
2024-04-17 23:14:18 -0400

    display pet image in results
    displays image of pet based on animalType

--- src/Results.js
+++ src/Results.js
@@ -1,15 +1,36 @@
 import React, { useState } from 'react';
 
+//get image url based on animal type
+function getImageUrl(animalType) {
+    switch(animalType){
+        case 'dog':
+            return require("../src/img/dog.jpeg");
+        case 'cat':
+            return require("../src/img/cat.jpeg");
+        case 'turtle':
+            return require("../src/img/turtle.jpeg");
+        case 'bunny':
+            return require("../src/img/bunny.webp");
+    }
+}
+
+//calculate race time based on form data
+function calcRaceTime(formData){
+
+}
+
 
 function Results({ formData  }) {
+    const petImageUrl = getImageUrl(formData.animalType);
+
     return (
     <div id="results">
         <h1>Race Results</h1>
         
         <div id="your-pet">
             <h2>Your Pet!</h2>
+            <img src={petImageUrl} alt="your pet" />
             <p>Name: {formData.petName}</p>
-            <p>Animal type: {formData.animalType}</p>
             <p>Diet: {formData.dietType}</p>
             <p>Exercise level: {formData.exerciseLevel}</p>
             <p>Race time: </p>
