edf0823bc0c96d12ba85ee9a4b16c4d49e800896
Perez0002 <op90210@gmail.com>
2024-04-19 00:32:57 -0400

    Added appropiate button navigation to pages based on mockup

--- src/components/ManageEventPage.jsx
+++ src/components/ManageEventPage.jsx
@@ -3,11 +3,22 @@ import { useNavigate } from "react-router-dom";
 import GuestListComponent from "./GuestListComponent.jsx";
 
 function ManageEventPage() {
-  
+    const navigate = useNavigate()
+    const handleMainPage = async (event) => {
+        event.preventDefault();
+        navigate("/main");
+    }
+
+    const handleManageEventPage = async (event) => {
+        event.preventDefault();
+        navigate("/event");
+    }
   return(
-    <div>
-      <GuestListComponent/>
-    </div>
+      <div>
+          <GuestListComponent/>
+          <button style={{marginLeft: '10px', marginTop: '10px', backgroundColor: 'rgb(178, 114, 238)', color: 'black', fontWeight: 'bold'}} className="btn waves-effect waves-light" type="button" id="mainPageButton" onClick={handleMainPage}>Main Page</button>
+          <button style={{marginLeft: '10px', marginTop: '10px', backgroundColor: 'rgb(178, 114, 238)', color: 'black', fontWeight: 'bold'}} className="btn waves-effect waves-light" type="button" id="eventPageButton" onClick={handleMainPage}>Back to Event Page</button>
+      </div>
   );
 }
 
