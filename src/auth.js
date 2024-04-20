fd23b43a091f7d2ad7216c798f31660921fee04f
ibixler <108148378+ibixler@users.noreply.github.com>
2024-04-20 17:59:13 -0400

    errgggegeggg
    this is broken
    
    promices and such are being a pain

--- src/auth.js
+++ src/auth.js
@@ -1,7 +1,35 @@
-/* const dotenv = require('dotenv')
+const dotenv = require('dotenv')
 const jwt = require('jsonwebtoken');
 dotenv.config();
+const bcrypt = require('bcryptjs');
 
+/*
+const bcrypt = require(‘bcryptjs’);
+// User registration
+const register = async (req, res) => {
+ const { name, email, password } = req.body;
+ const salt = await bcrypt.genSalt(10);
+ const hashedPassword = await bcrypt.hash(password, salt);
+ const tempObj = { name, email, password: hashedPassword };
+// Store the user object in the database
+ // …
+res.status(201).json({ message: 'Registration successful' });
+}; */
+
+exports.genHashSalt = async function (body) {
+    const password = body.password;
+    const salt = await bcrypt.genSalt(10);
+    if(password === null) return false;
+    const hashedPW = await bcrypt.hash(password, salt);
+    return hashedPW;
+
+}
+async function testHashSalt() {
+    const hashedPassword = await genHashSalt({ password: "farts" });
+    console.log(hashedPassword);
+}
+
+testHashSalt().catch(console.error);
 exports.generateAccessToken = function(username){
   return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '1800s' });
 }
@@ -18,4 +46,4 @@ exports.authenticateToken = function(req, res, next){
     console.log(user)
     next()
   })
-} */
+}
