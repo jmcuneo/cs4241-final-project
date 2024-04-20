ee1879d70a8d78b2380afe8efa37ae15cc44ca6e
ibixler <108148378+ibixler@users.noreply.github.com>
2024-04-20 15:35:05 -0400

    some basic additions, successful connection
    i ran prettier on this that was kind of a mistake oops... formatting looks a bit different, sorry, that is my bad

--- src/server.js
+++ src/server.js
@@ -1,75 +1,85 @@
-const express = require( 'express' ),
-      app = express(),
-      path = require('path')
+const express = require("express"),
+  app = express(),
+  path = require("path");
 
-require('dotenv').config()
+require("dotenv").config();
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`;
+const { connect } = require("http2");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+/* const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@${process.env.HOST}`;
+ */
+const uri = `mongodb+srv://ibixler:${process.env.PASS}@matchinglgbt.zyq3dy3.mongodb.net/?retryWrites=true&w=majority&appName=MatchingLGBT`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
+  },
+});
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+
+let collection = null,
+  dbName = "matching-game",
+  collectionName = "users";
+
+async function run() {
+  console.log("trying to connect");
+  await client.connect();
+
+  collection = await client.db(dbName).collection(collectionName);
+  console.log("connecting");
+  app.get("/docs", async (req, res) => {
+    if (collection !== null) {
+      const docs = await collection.find({}).toArray();
+      res.json(docs);
+    }
+  });
+}
+run();
+
+app.use((req, res, next) => {
+  if (collection !== null) {
+    next();
+  } else {
+    res.status(503).send();
   }
 });
 
-let collection = null,
-    dbName = "CHANGE ME",
-    collectionName = "CHANGEMETOO"
+app.post("/add", async (req, res) => {
+  console.log(req.body);
+  const result = await collection.insertOne(req.body);
+  /* res.json(result); */
+  res.status(200).send({ message : "user"});
+});
 
-async function run() {
+app.post("/remove", async (req, res) => {
+  const result = await collection.deleteOne({
+    _id: new ObjectId(req.body._id),
+  });
 
+  res.json(result);
+});
 
-    await client.connect();
-  
-    collection = await client.db(dbName).collection(collectionName);
-  
-    app.get("/docs", async (req, res) => {
-      if (collection !== null) {
-        const docs = await collection.find({}).toArray()
-        res.json(docs)
-      }
-    })
-  }
-  run()
-  
-  app.use((req, res, next) => {
-    if (collection !== null) {
-      next()
-    } else {
-      res.status(503).send()
+app.post("/update", async (req, res) => {
+  const result = await collection.updateOne(
+    { _id: new ObjectId(req.body._id) },
+    {
+      $set: {
+        /*json here*/
+      },
     }
-  })
-  
-  app.post('/add', async (req, res) => {
-    const result = await collection.insertOne(temp)
-    res.json(result)
-  })
-  
-  app.post('/remove', async (req, res) => {
-    const result = await collection.deleteOne({
-      _id: new ObjectId(req.body._id)
-    })
-  
-    res.json(result)
-  })
-  
-  app.post('/update', async (req, res) => {
-    const result = await collection.updateOne(
-      { _id: new ObjectId(req.body._id) },
-      { $set: { /*json here*/} }
-    )
-  
-    res.json(result)
-  })
-  //DATABASE CONNECTION END
+  );
 
+  res.json(result);
+});
+//DATABASE CONNECTION END
+client.close();
 
-
-app.listen( process.env.PORT || 3000 )
+app.listen(process.env.PORT || 3000);
