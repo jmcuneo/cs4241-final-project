dedd66a1ebaee2cfdf8bae820a625993c0f2959e
Jayson <113391142+JaysonCaissie@users.noreply.github.com>
2024-04-23 19:57:24 -0400

    More secure sessions. jwt encoding and decoding

--- src/App.jsx
+++ src/App.jsx
@@ -8,29 +8,53 @@ import ManageEventPage from './components/ManageEventPage';
 import RegisterPage from './components/RegisterPage';
 import ProfilePage from './components/ProfilePage';
 
+
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
-  console.log(token)
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const response = await fetch('/api/verifyToken', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ token }),
+        });
   
+        const data = await response.json();
+        console.log(data)
+        setAuthenticated(data.valid === true);
+      } catch (error) {
+        console.error('Error verifying token:', error);
+        setAuthenticated(false);
+      }
+    };
+  
+    verifyToken(); 
+  }, [token]);
+
   const handleLogin = (newToken) => {
     setToken(newToken);
-    localStorage.setItem('token', newToken); // Store token in localStorage
+    localStorage.setItem('token', newToken); //Store token in localStorage
   };
 
   const handleLogout = () => {
     setToken(null);
-    localStorage.removeItem('token'); // Remove token from localStorage
+    localStorage.removeItem('token'); //Remove token from localStorage
   };
 
   return (
     <Router>
       <Routes>
         {/*LOGIN AND REGISTER*/}
-        {!token && <Route exact path="/login" element={<LoginPage onLogin={handleLogin} />} />}
+        {!authenticated && <Route exact path="/login" element={<LoginPage onLogin={handleLogin} />} />}
         <Route exact path="/register" element={<RegisterPage onRegister={handleLogin}/>} />
 
         {/*PROTECTED ROUTES*/}
-        {token && (
+        {authenticated && (
           <>
             <Route exact path="/main" element={<MainPage onLogout={handleLogout}/>} />
             <Route exact path="/event" element={<EventPage />} />
@@ -39,7 +63,7 @@ function App() {
           </>
         )}
         {/* Redirect to main page if authenticated */}
-        {token && <Route path="*" element={<Navigate to="/main" />} />}
+        {authenticated && <Route path="*" element={<Navigate to="/main" />} />}
         {/* Redirect to login page if not authenticated */}
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
