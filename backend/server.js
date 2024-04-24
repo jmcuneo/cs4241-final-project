c106d6a48d5d42c706aae5951c0b1db7680e10aa
Hanzalah Qamar <hanzalah.qamar02@gmail.com>
2024-04-24 12:09:26 -0400

    set up getting users for sidebar

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js"; 
 import messageRoutes from "./routes/message.routes.js"; 
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
+
 const PORT = process.env.PORT || 8000;
 const app =  express();
 
@@ -14,6 +18,7 @@ app.use(cookieParser()); //parse cookies from the request headers
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => { 
 //     //root route
