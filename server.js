ec890f842373a693413dce647dbb0843708efe7d
ComputerCow <mjacobs2@wpi.edu>
2024-04-25 12:21:01 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -27,8 +27,10 @@ io.on('connection', (socket) => {
     io.to(room).emit('message receive', "Server", message);
   }
   socket.on('try host game', async(room)=>{
+    console.log("Try host game called");
     room = room.toLowerCase();
     const existingGame = await database.getGameByRoomCode(room);
+    // console.log("")
     if(existingGame != null){
       socket.emit('room available');
     }else{
