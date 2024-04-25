9677e7307ef3ee7d7676f2432c0abc6a47b8b700
Perez0002 <op90210@gmail.com>
2024-04-25 13:29:00 -0400

    input, submission, back button implemented - waiting on database connection

--- src/components/AddGuest.jsx
+++ src/components/AddGuest.jsx
@@ -1,35 +1,29 @@
 import React, { useState, useRef } from 'react';
 import Popup from 'reactjs-popup';
-import { useNavigate, useParams } from "react-router-dom";
-import PropTypes from 'prop-types';
 
 function AddGuest(){
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //add to database
+    }
+
     return(
         <div>
-            <Popup trigger=
-                       {<button> Add Guest </button>}
-                   modal nested>
-                {
-                    close => (
-                        <div className='modal'>
-                            <div className='content'>
-                                <input type="text"><input/>
-                            </div>
-                            <div>
-                                <button onClick=
-                                            {() => close()}>
-                                    Back
-                                </button>
-                                <button onSubmit={}>
-                                    Submit
-                                </button>
-                            </div>
+            <Popup trigger={<button> Add Guest </button>} modal nested>
+                {close => (
+                    <div className='modal'>
+                        <div className='content'>
+                            <form onSubmit={handleSubmit}>
+                                <input type="text" />
+                                <button onClick={() => close()}>Back</button>
+                                <button type="submit">Submit</button>
+                            </form>
                         </div>
-                    )
-                }
+                    </div>
+                )}
             </Popup>
         </div>
-)
+    )
 }
 
 export default AddGuest;
