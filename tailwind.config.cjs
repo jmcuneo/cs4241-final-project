1de7de3c0e990f560f20b62aa8554f7f52e14c3c
Sargeanthost <sargeanthost@gmail.com>
2024-04-26 18:10:04 -0400

    force dark mode. get rid of index.css comment. can look at previous commits if needed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,7 @@
 module.exports = {
     content: ['./src/**/*.{js,ts,jsx,tsx}'],
     plugins: [require("@tailwindcss/typography") ,require('daisyui')],
-    theme: {
-        container: {
-            center: true,
-        },
-    },
+    daisyui:{
+        themes:["dark"]
+    }
 };
