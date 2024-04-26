cc951b365ef10e04bc42495eb95f3879ce531ee9
Hanzalah Qamar <hanzalah.qamar02@gmail.com>
2024-04-26 03:10:44 -0400

    set up login and sign up UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/login/Login.jsx'
 import Signup from "./pages/signup/SignUp.jsx"
 import Home from './pages/home/Home.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showLogin, setShowLogin] = useState(true);  // State to toggle views
 
   return (
-    <div className='p-4 h-screen flex items-center justify-center'>
-      <Login/>
-      <Signup/>
-      <Home/>
+    <div>
+      <div className='p-4 h-screen flex items-center justify-center'>
+        {showLogin ? <Login onSignUpClick={() => setShowLogin(false)} /> : <Signup onLoginClick={() => setShowLogin(true)} />}
+      </div>
+      <div>
+        {/* <Home/>  */}
+      </div>
     </div>
+
   )
 }
 
