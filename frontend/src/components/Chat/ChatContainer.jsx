152c37d33c9b3b4ff903a141741f604d1a59ba41
Hanzalah Qamar <hanzalah.qamar02@gmail.com>
2024-04-26 22:47:33 -0400

    set up message box and input

--- frontend/src/components/Chat/ChatContainer.jsx
+++ frontend/src/components/Chat/ChatContainer.jsx
@@ -1,15 +1,52 @@
 import React from 'react';
 import OtherUserCard from './OtherUserCard';
 import MessageBox from './MessageBox';
+import Message from './Message';
 
 const ChatContainer = () => {
   return (
     <div className="flex flex-col h-screen">
-        <OtherUserCard />
-        <div className="flex-grow">
-            actual chats rendered here
+      <OtherUserCard />
+      <div className="flex-grow overflow-auto pt-4 pb-4">
+
+        <div className="flex flex-col items-start gap-4">
+          <div className="chat chat-start w-full">
+            <Message
+              text="YOU TURNED HER AGAINST ME!"
+              username="Anakin"
+              time="12:25"
+              isStart={true}
+            />
+          </div>
+          <div className="chat chat-end w-full">
+            <Message
+              text="You have done that yourself."
+              username="Obi-Wan"
+              time="12:35"
+              isStart={false}
+            />
+          </div>
+         
+          <div className="chat chat-end w-full">
+            <Message
+              text="You underestimate my power!"
+              username="Obi-Wan"
+              time="12:35"
+              isStart={false}
+            />
+          </div>
+          <div className="chat chat-end w-full">
+            <Message
+              text="You underestimate my power!"
+              username="Obi-Wan"
+              time="12:35"
+              isStart={false}
+            />
+          </div>
         </div>
-        <MessageBox />
+
+      </div>
+      <MessageBox />
     </div>
   );
 }
