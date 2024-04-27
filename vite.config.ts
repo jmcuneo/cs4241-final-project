317927d5fd6d53c7be673d403032887842f45322
Sicard, Lucas J (901004442) <lsicard@wpi.edu>
2024-04-27 16:13:42 -0400

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  server: {
+    host: true,
+    strictPort: true,
+    port: 8000,
+  },
 })
