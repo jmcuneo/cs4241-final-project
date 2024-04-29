8f0914e6bf04c6081dfa6c56434581d372a5652f
AlexanderBeck0 <25886582+AlexanderBeck0@users.noreply.github.com>
2024-04-28 23:20:10 -0400

    Fixed an infinite request loop

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import PropTypes from "prop-types";
 import Navbar from "./Navbar";
 import TableComponent from "./TableComponent";
@@ -41,14 +41,14 @@ function MainPage({ onLogout, isAdmin }) {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ 
+        body: JSON.stringify({
           token: localStorage.getItem("token"),
           eventBody: {
             name: eventName,
             date: eventDate,
             location: eventLocation
           }
-       }),
+        }),
       });
 
       const res = await response.json();
@@ -58,26 +58,25 @@ function MainPage({ onLogout, isAdmin }) {
       console.error("Error creating event:", error);
     }
   };
+  const getUpcomingEvents = useCallback(async () => {
+    try {
+      const response = await fetch("//localhost:3000/api/getUpcomingEvents", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ token: localStorage.getItem("token") }),
+      });
 
+      const _events = await response.json();
+      setEvents(_events);
+    } catch (error) {
+      console.error("Error getting events:", error);
+    }
+  }, []);
   useEffect(() => {
-    const getUpcomingEvents = async () => {
-      try {
-        const response = await fetch("//localhost:3000/api/getUpcomingEvents", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ token: localStorage.getItem("token") }),
-        });
-
-        const _events = await response.json();
-        setEvents(_events);
-      } catch (error) {
-        console.error("Error getting events:", error);
-      }
-    };
     getUpcomingEvents();
-  }, [events]);
+  }, [getUpcomingEvents]);
   events.map((row) => {
     new Date(row.date).toLocaleDateString(navigator.languages, {
       day: "numeric",
@@ -92,93 +91,93 @@ function MainPage({ onLogout, isAdmin }) {
   );
 
   return (
-  <>
-    <Navbar onLogout={onLogout} showProfileButton={true}></Navbar>
-    <motion.div
-    initial={{ scale: 0 }}
-    animate={{ scale: 1 }}
-    transition={{
-      type: "spring",
-      stiffness: 260,
-      damping: 20,
-    }}
-    >
-      <div className="main-page-container relative flex min-h-max flex-col mt-10 mx-auto items-center prose">
-        <div className="max-h-[75vh] overflow-y-auto flex flex-row">
-          <h1>Upcoming Events</h1>
-        </div>
-        <div className="max-h-[75vh] overflow-y-auto flex flex-col">
-          <input
-              style={{marginBottom: "-20px", width: '100%'}}
+    <>
+      <Navbar onLogout={onLogout} showProfileButton={true}></Navbar>
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+        }}
+      >
+        <div className="main-page-container relative flex min-h-max flex-col mt-10 mx-auto items-center prose">
+          <div className="max-h-[75vh] overflow-y-auto flex flex-row">
+            <h1>Upcoming Events</h1>
+          </div>
+          <div className="max-h-[75vh] overflow-y-auto flex flex-col">
+            <input
+              style={{ marginBottom: "-20px", width: '100%' }}
               type="text"
               placeholder="Search by Event name"
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
               className="input input-bordered w-full mb-4"
             />
-          <div className="flex-grow">
-            <TableComponent
-              headers={[
-                "Name",
-                "Date",
-                "Location",
-                "Guest Count",
-                "User Invites",
-              ]}
-              rows={filteredEvents}
-              isEvent={true}>
-            </TableComponent>
+            <div className="flex-grow">
+              <TableComponent
+                headers={[
+                  "Name",
+                  "Date",
+                  "Location",
+                  "Guest Count",
+                  "User Invites",
+                ]}
+                rows={filteredEvents}
+                isEvent={true}>
+              </TableComponent>
+            </div>
           </div>
         </div>
-      </div>
-      {isAdmin && (
-        <div className="add-guest flex justify-center align-center max-h-[75vh] overflow-y-auto">
-          <form className="w-full" onSubmit={(e) => handleCreateEvent(e, eventNameInput.current.value, formatEventDate(eventDateInput.current.value), eventLocationInput.current.value)}>
-            
-            <div className="flex flex-col justify-start items-center">
-              <h1>Create a New Event</h1>
-              <input
-                className="input input-bordered w-full max-w-xs"
-                type="text"
-                id="eventName"
-                placeholder="Event Name"
-                style={{margin:"0.5rem"}}
-                required
-                ref={eventNameInput}
-              />
-              <input
-                className="input input-bordered w-full max-w-xs"
-                type="datetime-local"
-                id="eventDate"
-                placeholder="Event Date"
-                style={{margin:"0.5rem"}}
-                required
-                ref={eventDateInput}
-              />
-              <input
-                className="input input-bordered w-full max-w-xs"
-                type="text"
-                id="eventLocation"
-                placeholder="Event Location"
-                style={{margin:"0.5rem"}}
-                required
-                ref={eventLocationInput}
-              />
-              <button
-              className="btn btn-success add-guest-button mt-2"
-              type="submit"
-              id="createEventButton"
-              style={{marginLeft:"1rem"}}
-            >
-              Create New Event
-            </button>
-            </div>
-          </form>
-          <div className="text-xl ml-7 mt-2.5 color-white">{message}</div>
-        </div>
-      )}
-    </motion.div>
-  </>
+        {isAdmin && (
+          <div className="add-guest flex justify-center align-center max-h-[75vh] overflow-y-auto">
+            <form className="w-full" onSubmit={(e) => handleCreateEvent(e, eventNameInput.current.value, formatEventDate(eventDateInput.current.value), eventLocationInput.current.value)}>
+
+              <div className="flex flex-col justify-start items-center">
+                <h1>Create a New Event</h1>
+                <input
+                  className="input input-bordered w-full max-w-xs"
+                  type="text"
+                  id="eventName"
+                  placeholder="Event Name"
+                  style={{ margin: "0.5rem" }}
+                  required
+                  ref={eventNameInput}
+                />
+                <input
+                  className="input input-bordered w-full max-w-xs"
+                  type="datetime-local"
+                  id="eventDate"
+                  placeholder="Event Date"
+                  style={{ margin: "0.5rem" }}
+                  required
+                  ref={eventDateInput}
+                />
+                <input
+                  className="input input-bordered w-full max-w-xs"
+                  type="text"
+                  id="eventLocation"
+                  placeholder="Event Location"
+                  style={{ margin: "0.5rem" }}
+                  required
+                  ref={eventLocationInput}
+                />
+                <button
+                  className="btn btn-success add-guest-button mt-2"
+                  type="submit"
+                  id="createEventButton"
+                  style={{ marginLeft: "1rem" }}
+                >
+                  Create New Event
+                </button>
+              </div>
+            </form>
+            <div className="text-xl ml-7 mt-2.5 color-white">{message}</div>
+          </div>
+        )}
+      </motion.div>
+    </>
   );
 }
 
--- src/components/UserListComponent.jsx
+++ src/components/UserListComponent.jsx
@@ -1,156 +1,150 @@
-import { useEffect, useState, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
-import PropTypes from "prop-types";
-import GuestListComponent from "./GuestListComponent";
 
 function UserListComponent() {
-    const { eventId } = useParams();
-    const [userList, setUserList] = useState([]);
-    const guestNameRef = useRef(null);
-    const [message, setMessage] = useState("");
-    const addInviterInput = useRef(null);
+  const { eventId } = useParams();
+  const [userList, setUserList] = useState([]);
+  const guestNameRef = useRef(null);
+  const [message, setMessage] = useState("");
+  const addInviterInput = useRef(null);
 
-    //TODO: add pop up messages to each input field conirming a succesfull change
+  //TODO: add pop up messages to each input field conirming a succesfull change
 
-    const getUserList = async () => {
-        try {
-          const response = await fetch("//localhost:3000/api/getAllowedInviters", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              token: localStorage.getItem("token"),
-              eventId: eventId,
-            }),
-          });
-    
-          const users = await response.json();
-          setUserList(users);
-        } catch (error) {
-          console.error("Error getting users:", error);
-        }
-      };
+  const getUserList = useCallback(async () => {
+    try {
+      const response = await fetch("//localhost:3000/api/getAllowedInviters", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: localStorage.getItem("token"),
+          eventId: eventId,
+        }),
+      });
 
-    const handleRemove = async (username) => {
-      try {
-        const response = await fetch("//localhost:3000/api/removeAllowedInviter", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            token: localStorage.getItem("token"),
-            eventId: eventId,
-            inviterName: username,
-          }),
-        });
-        console.log(await response.json());
+      const users = await response.json();
+      setUserList(users);
+    } catch (error) {
+      console.error("Error getting users:", error);
+    }
+  }, [eventId]);
 
-      } catch (error) {
-        console.error("Error removing inviter:", error);
-      }
-    };
+  const handleRemove = async (username) => {
+    try {
+      const response = await fetch("//localhost:3000/api/removeAllowedInviter", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: localStorage.getItem("token"),
+          eventId: eventId,
+          inviterName: username,
+        }),
+      });
+      console.log(await response.json());
 
-    const handleAdd = async () => {
-      try {
-        const response = await fetch("//localhost:3000/api/addAllowedInviter", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            token: localStorage.getItem("token"),
-            eventId: eventId,
-            inviterName: addInviterInput.current.value,
-          }),
-        });
-        console.log(await response.json());
+    } catch (error) {
+      console.error("Error removing inviter:", error);
+    }
+  };
 
-      } catch (error) {
-        console.error("Error adding inviter:", error);
-      }
+  const handleAdd = async () => {
+    try {
+      const response = await fetch("//localhost:3000/api/addAllowedInviter", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          token: localStorage.getItem("token"),
+          eventId: eventId,
+          inviterName: addInviterInput.current.value,
+        }),
+      });
+      console.log(await response.json());
 
-      addInviterInput.current.value = '';
-    };
+    } catch (error) {
+      console.error("Error adding inviter:", error);
+    }
 
-    useEffect(() => {
-        getUserList();
-    }, [eventId, userList]);
+    addInviterInput.current.value = '';
+  };
 
-    const guestTable = () => {
-        return (
-          <div className="flex justify-center align-center overflow-y-auto">
-            <table className="table table-zebra bg-neutral not-prose table-md  w-full">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Username</th>
-                  <th>Guest Count</th>
-                </tr>
-              </thead>
-              <tbody>
-                {userList.map((user, index) => (
-                  <tr key={index}>
-                    <td>{user.fullName}</td>
-                    <td>{user.username}</td>
-                    <td>{user.guestCount}</td>
-                    <td>
-                      <button
-                        className="btn bg-red-500 hover:bg-red-600 text-black font-bold "
-                        type="button"
-                        id={"removeButton" + index}
-                        onClick={() => handleRemove(user.username)}
-                      >
-                        Remove
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        );
-    };
+  useEffect(() => {
+    getUserList();
+  }, [getUserList]);
 
+  const guestTable = () => {
     return (
-        <div className="prose min-w-screen">
-            <div className="flex flex-col px-2  w-screen">
-              <div className="flex overflow-y-auto">
-                <h1 className="w" style={{marginLeft: "1rem", marginBottom: "1rem", marginTop: "1rem"}}>Inviter List</h1>
-                <input
-                  className="validate input input-bordered w-full max-w-xs max-h-9 input-primary focus:outline-accent ml-2"
-                  type="text"
-                  id="addInviterInput"
-                  name="addInviterInput"
-                  placeholder="input username"
-                  style={{marginTop:"1.5rem", marginLeft:"1.5rem"}}
-                  ref={addInviterInput}
-                />
-                <button
-                  className="btn bg-green-500 hover:bg-green-600 text-black font-bold "
-                  type="button"
-                  id={"addButton"}
-                  style={{margin: "1rem"}}
-                  onClick={() => handleAdd()}
-                >
-                  Add Inviter
-                </button>
-              </div>
-                <div className="grid columns-3 grid-cols-3 gap-1">
-                    {userList.length > 0 ? (
-                        guestTable()
-                    ) : (
-                        <p className="text-slate text-center w-full">No allowed inviters</p>
-                    )}
-                </div>
-            </div>
-        </div>
+      <div className="flex justify-center align-center overflow-y-auto">
+        <table className="table table-zebra bg-neutral not-prose table-md  w-full">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Guest Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userList.map((user, index) => (
+              <tr key={index}>
+                <td>{user.fullName}</td>
+                <td>{user.username}</td>
+                <td>{user.guestCount}</td>
+                <td>
+                  <button
+                    className="btn bg-red-500 hover:bg-red-600 text-black font-bold "
+                    type="button"
+                    id={"removeButton" + index}
+                    onClick={() => handleRemove(user.username)}
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
-}
+  };
 
-UserListComponent.propTypes = {
-    onUpdate: PropTypes.func,
-};
+  return (
+    <div className="prose min-w-screen">
+      <div className="flex flex-col px-2  w-screen">
+        <div className="flex overflow-y-auto">
+          <h1 className="w" style={{ marginLeft: "1rem", marginBottom: "1rem", marginTop: "1rem" }}>Inviter List</h1>
+          <input
+            className="validate input input-bordered w-full max-w-xs max-h-9 input-primary focus:outline-accent ml-2"
+            type="text"
+            id="addInviterInput"
+            name="addInviterInput"
+            placeholder="input username"
+            style={{ marginTop: "1.5rem", marginLeft: "1.5rem" }}
+            ref={addInviterInput}
+          />
+          <button
+            className="btn bg-green-500 hover:bg-green-600 text-black font-bold "
+            type="button"
+            id={"addButton"}
+            style={{ margin: "1rem" }}
+            onClick={() => handleAdd()}
+          >
+            Add Inviter
+          </button>
+        </div>
+        <div className="grid columns-3 grid-cols-3 gap-1">
+          {userList.length > 0 ? (
+            guestTable()
+          ) : (
+            <p className="text-slate text-center w-full">No allowed inviters</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default UserListComponent;
