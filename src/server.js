bc6a25a7de1311cbe221416feb3a1a5821617999
ibixler <108148378+ibixler@users.noreply.github.com>
2024-04-29 14:14:19 -0400

    rahhhhh

--- src/server.js
+++ src/server.js
@@ -33,7 +33,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://127.0.0.1:3000/auth/github/callback",
+      callbackURL: process.env.REDIRECT,
     },
     function (accessToken, refreshToken, profile, cb) {
       console.log("at: ", accessToken, "rt: ", refreshToken, profile, cb);
